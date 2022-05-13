3183854c496a250cd920a290cb98def2f4046d50
Andrew Bell <fiixed@gmail.com>
2022-05-13 09:42:02 -0400

    game ui complete

---
+++ lib/pages/game_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatelessWidget {
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.05),
+          child: _gameUI(),
+        ),
+      ),
+    );
+  }
+
+  Widget _gameUI() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _questionText(),
+        Column(
+          children: [
+            _trueButton(),
+            SizedBox(
+              height: _deviceHeight! * 0.01,
+            ),
+            _falseButton(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _questionText() {
+    return const Text(
+      'Test Question 1, Nothing interesting',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _trueButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.green,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        'True',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+
+  Widget _falseButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.red,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        'False',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
