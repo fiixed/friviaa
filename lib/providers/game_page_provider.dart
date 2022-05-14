d0b30882ee3787ca414e977b1a1f772ea1467b8a
Andrew Bell <fiixed@gmail.com>
2022-05-14 08:16:29 -0400

    dio fetch

--- lib/providers/game_page_provider.dart
+++ lib/providers/game_page_provider.dart
@@ -1,12 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class GamePageProvider extends ChangeNotifier {
   final Dio _dio = Dio();
+  final int _maxQuestions = 10;
+
+  List? questions;
 
   BuildContext context;
   GamePageProvider({required this.context}) {
     _dio.options.baseUrl = 'https://opentdb.com/api.php';
-    print("Hello");
+    _getQuestionsFromAPI();
+  }
+
+  Future<void> _getQuestionsFromAPI() async {
+    var _response = await _dio.get(
+      '',
+      queryParameters: {
+        'amount': 10,
+        'type': 'boolean',
+      },
+    );
+    var _data = jsonDecode(
+      _response.toString(),
+    );
+    questions = _data['results'];
+    print(questions);
   }
 }
