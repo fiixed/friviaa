7b76e4d981c647567d3785da2d1c778d5d7e6363
Andrew Bell <fiixed@gmail.com>
2022-05-16 07:52:04 -0400

    correct answer

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -70,7 +70,9 @@ class GamePage extends StatelessWidget {
 
   Widget _trueButton() {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: () {
+        _pageProvider?.answerQuestion('True');
+      },
       color: Colors.green,
       minWidth: _deviceWidth! * 0.80,
       height: _deviceHeight! * 0.10,
@@ -86,7 +88,9 @@ class GamePage extends StatelessWidget {
 
   Widget _falseButton() {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: () {
+        _pageProvider?.answerQuestion('False');
+      },
       color: Colors.red,
       minWidth: _deviceWidth! * 0.80,
       height: _deviceHeight! * 0.10,
--- lib/providers/game_page_provider.dart
+++ lib/providers/game_page_provider.dart
@@ -34,4 +34,12 @@ class GamePageProvider extends ChangeNotifier {
   String getCurrentQuestionText() {
     return questions![_currentQuestionCount]['question'];
   }
+
+  void answerQuestion(String _answer) async {
+    bool isCorrect =
+        questions![_currentQuestionCount]['correct_answer'] == _answer;
+    _currentQuestionCount++;
+    print(isCorrect ? "Correct" : "Incorrect");
+    notifyListeners();
+  }
 }
